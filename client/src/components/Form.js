4e4a8a88d7c5d83c34ce8000cb2d9865f7b64475
Reuben T Allik <reubenallik@gmail.com>
2021-04-02 22:32:37 -0400

    node-sass (depricated) to dart-sass

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -14,15 +14,16 @@ const Form = () => {
         console.count('form - set location')
         console.log("******************", e, '***********', location, "************")
         console.log(input.current.value)
-        setLoading(true);
-        setLocation(input.current.value)
-        
+        if (input.current.value !== '') {
+            setLoading(true);
+            setLocation(input.current.value)
+        }
     }
 
     return (
         <form className='loc-search' onSubmit={handleSubmit}>
             <label htmlFor="test">Enter a Location: </label>
-            <input type="text" id="test" name="test" ref={input}/>
+            <input type="text" id="test" name="test" ref={input} required/>
             <button type='submit'>Submit</button>
         </form>
     )
