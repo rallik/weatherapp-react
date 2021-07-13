d15419f5775d90f2b8c5416fb69fa02eda199f2d
Reuben T Allik <reubenallik@gmail.com>
2021-07-13 16:36:49 -0400

    need component refactor

--- client/src/components/LocTime.js
+++ client/src/components/LocTime.js
@@ -3,12 +3,14 @@ import SunSched from './SunSched'
 import { useGlobalContext } from '../context';
 import { convertTime } from './functions';
 
-const Time = () => {
-    const { currentWeather: { dt, sys, timezone, coord } } = useGlobalContext();
+const LocTime = () => {
+    const { currentWeather: { dt, sys, timezone, coord, name } } = useGlobalContext();
     const location_time = convertTime(dt, false, timezone);
     
     return (
-        <div className="time">
+        <div className="loc-time">
+            <h1 className='city'>{name}</h1>
+
             <h4 className='time-updated'>Last Updated:&nbsp;</h4>
             <p className='time-updated'>{location_time.t_display}</p>
             <SunSched props={{ sys, coord, location_time, timezone }} />
@@ -16,5 +18,5 @@ const Time = () => {
     )
 }
 
-export default Time
+export default LocTime
 
