a096897ccb998a3c8d76ee86aeb1d77d4d775db5
Reuben T Allik <reubenallik@gmail.com>
2021-07-14 14:08:28 -0400

    sunbar started

---
+++ client/src/components/SunBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SunBar = ({ sunwindow }) => {
+
+    console.log(sunwindow)
+
+
+
+
+
+    return (
+        <div>
+            Sunbar
+        </div>
+    )
+}
+
+export default SunBar
